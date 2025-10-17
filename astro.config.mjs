14f4f0ba980d522d08260aeed43906ef44521c49
Jonathan <contact@grafikart.fr>
2025-10-17 11:43:07 +0200

    First commit

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    trailingSlash: 'never',
+	integrations: [mdx(), sitemap()],
+    site: 'https://grafikart.github.io',
+    base: '/test-astro',
+});
